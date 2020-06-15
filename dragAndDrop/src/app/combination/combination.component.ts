3724222e6c8e77c2e83cb1b6e84b3fa5b7824eb3
daeun <programmerdaeun@gmail.com>
2020-06-15 11:20:03 +0900

    기본 세팅 - css - drag and drop

--- dragAndDrop/src/app/combination/combination.component.ts
+++ dragAndDrop/src/app/combination/combination.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import interact from 'interactjs';
 
 @Component({
   selector: 'app-combination',
@@ -7,9 +8,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CombinationComponent implements OnInit {
 
+  public movedElement = '';
+  public clickedDiv = '';
+  public setW = 0;
+  public setH = 0;
+
   constructor() { }
 
   ngOnInit() {
+    interact('.childDiv')
+      .resizable({
+        edges: {
+          bottom: true,
+          top: true,
+          left: true,
+          right: true
+        },
+        // The bind function returns a new function that is bound to the this you defined.
+        onmove: this.resizeMoveListener.bind(this),
+        inertia: true
+      })
+      .draggable({
+        onmove: this.dragMoveListener.bind(this),
+        inertia: true
+      });
+  }
+
+  private dragMoveListener(event) {
+    const target = event.target;
+    this.movedElement = target.id;
+
+    // keep the dragged position in the data-x/data-y attributes
+    let x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+    let y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.transform = `translate(${x}px, ${y}px)`;
+
+    // update the position attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+  private resizeMoveListener(event) {
+    const target = event.target;
+    this.movedElement = target.id;
+
+    let x = (parseFloat(target.getAttribute('data-x')) || 0);
+    let y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+    // update the element's style
+    target.style.width = `${event.rect.width}px`;
+    target.style.height = `${event.rect.height}px`;
+
+    this.setW = event.rect.width / 2;
+    this.setH = event.rect.height / 2;
+
+    // translate when resizing from top or left edges
+    x += event.deltaRect.left;
+    y += event.deltaRect.top;
+
+    target.style.transform = `translate(${x}px, ${y}px)`;
+
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+    target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height);
   }
 
 }
