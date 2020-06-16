7e618fbce1ec6129f948ac408f9aa1d5d86c89ea
daeun <programmerdaeun@gmail.com>
2020-06-16 13:39:31 +0900

    page 나누기

--- dragAndDrop/src/app/combination/combination.component.ts
+++ dragAndDrop/src/app/combination/combination.component.ts
@@ -1,5 +1,9 @@
-import {Component, HostListener, OnInit} from '@angular/core';
-import interact from 'interactjs';
+import {Component, OnInit} from '@angular/core';
+
+export enum PAGE {
+  ONE = 1,
+  TWO = 2
+}
 
 @Component({
   selector: 'app-combination',
@@ -8,131 +12,15 @@ import interact from 'interactjs';
 })
 export class CombinationComponent implements OnInit {
 
-  private clickedDiv: string;
-
-  @HostListener('document:mousemove', ['$event'])
-  onMouseMove(e) {
-    // console.log(e);
-  }
+  public selectedTab = PAGE.ONE;
+  public readonly PAGE = PAGE;
 
   constructor() { }
 
   ngOnInit() {
-
-    interact('.childDiv')
-      .resizable({
-        edges: {
-          bottom: true,
-          top: true,
-          left: true,
-          right: true
-        },
-        // The bind function returns a new function that is bound to the this you defined.
-        onmove: this.resizeMoveListener.bind(this),
-        inertia: true
-      })
-      .draggable({
-        onmove: this.dragMoveListener.bind(this),
-        inertia: true
-      });
   }
 
-  // drag and drop
-  private dragMoveListener(event) {
-    const target = event.target;
-
-    let x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
-    let y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-  }
-
-  private resizeMoveListener(event) {
-    const target = event.target;
-
-    let x = (parseFloat(target.getAttribute('data-x')) || 0);
-    let y = (parseFloat(target.getAttribute('data-y')) || 0);
-
-    target.style.width = `${event.rect.width}px`;
-    target.style.height = `${event.rect.height}px`;
-
-    x += event.deltaRect.left;
-    y += event.deltaRect.top;
-
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-    target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height);
-  }
-
-  // 클릭
-  public click(e: MouseEvent) {
-    this.clickedDiv = e.target[ 'id' ];
-    console.log(this.clickedDiv);
-  }
-
-  // 정렬
-  public up() {
-    if (this.clickedDiv) {
-      const childDiv = document.getElementById(this.clickedDiv.toString());
-      childDiv.style.top = '0px';
-    }
-  }
-
-  public down() {
-    if (this.clickedDiv) {
-      const parentDiv = document.getElementById('parentDiv');
-      const childDiv = document.getElementById(this.clickedDiv.toString());
-      childDiv.style.top = `${parentDiv.offsetHeight - childDiv.offsetHeight}px`;
-    }
-  }
-
-  // 옮기기
-  public top() {
-    if (this.clickedDiv) {
-      const parentDiv = document.getElementById('parentDiv');
-      const childDiv = document.getElementById(this.clickedDiv.toString());
-      parentDiv.insertAdjacentElement('beforeend', childDiv);
-    }
-  }
-
-  public bottom() {
-    if (this.clickedDiv) {
-      const parentDiv = document.getElementById('parentDiv');
-      const childDiv = document.getElementById(this.clickedDiv.toString());
-      parentDiv.insertAdjacentElement('afterbegin', childDiv);
-    }
-  }
-
-  // element 추가
-  private setColor() {
-    let x = Math.floor(Math.random() * 256);
-    let y = Math.floor(Math.random() * 256);
-    let z = Math.floor(Math.random() * 256);
-    return 'rgb(' + x + ',' + y + ',' + z + ')';
-  }
-
-  public addDiv0() {
-    let newDiv = document.createElement('div');
-    let bgColor = this.setColor();
-    newDiv.id = `childDiv${Math.floor(Math.random() * 100)}`;
-    newDiv.className = 'childDiv';
-    newDiv.setAttribute('style', `background-color: ${bgColor}; position: absolute; box-sizing: border-box; touch-action: none; width: 400px; height: 400px;`);
-    newDiv.addEventListener('click', (event) => {
-      this.click(event);
-    });
-
-    newDiv.textContent = 'new box!';
-    document.getElementById('parentDiv').appendChild(newDiv);
-  }
-
-  public addDiv1() {
-
+  public changeTab(page: PAGE) {
+    this.selectedTab = page;
   }
 }
