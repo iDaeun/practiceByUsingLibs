b4d32f1543e4a972bd3d337925482096233a6bd7
daeun <programmerdaeun@gmail.com>
2020-06-26 11:32:43 +0900

    route 구조 정리 - home, template

--- dragAndDrop/src/app/url-path.ts
+++ dragAndDrop/src/app/url-path.ts
@@ -1,10 +1,19 @@
 // 폴더 및 Route 구조 정의 ( 기획서 내용 참조 )
+// syncView, syncEdit, snapView, snapEdit
 export namespace UrlPath {
 
-  export class APP {
+  export class TEMPLATE {
     public static ROUTE = {
-      ROOT: 'app'
+      ROOT: 'template'
     };
+  }
+
+  export class HOME {
+    public static ROUTE = {
+      ROOT: 'home'
+    };
+
+    public static ROOT = '/' + TEMPLATE.ROUTE.ROOT + '/' + HOME.ROUTE.ROOT;
   }
 
 }
