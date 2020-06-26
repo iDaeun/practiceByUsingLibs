d57977656e6c565ef4098f95b5450262e9e69826
daeun <programmerdaeun@gmail.com>
2020-06-26 16:03:33 +0900

    route 구조 정리 - 수정

--- dragAndDrop/src/app/url-path.ts
+++ dragAndDrop/src/app/url-path.ts
@@ -2,18 +2,43 @@
 // syncView, syncEdit, snapView, snapEdit
 export namespace UrlPath {
 
-  export class TEMPLATE {
-    public static ROUTE = {
-      ROOT: 'template'
-    };
-  }
-
+  // 홈
   export class HOME {
     public static ROUTE = {
-      ROOT: 'home'
+      ROOT: 'home',
+      FOLDER: 'folder/:folderId', // 폴더 선택
+      TEMPLATE: 'templateList' // 템플릿 목록 화면
     };
 
-    public static ROOT = '/' + TEMPLATE.ROUTE.ROOT + '/' + HOME.ROUTE.ROOT;
+    public static ROOT = '/' + HOME.ROUTE.ROOT;
+    public static FOLDER = '/' + HOME.ROUTE.ROOT + '/' + HOME.ROUTE.FOLDER;
+    public static TEMPLATE = '/' + HOME.ROUTE.ROOT + '/' + HOME.ROUTE.TEMPLATE;
+  }
+
+  // 리포트 만들기 -> 싱크 리포트
+  export class SYNC {
+    public static ROUTE = {
+      ROOT: 'sync',
+      DETAIL: 'detail/:syncId', // 특정 sync report 열람
+      EDIT: 'edit/:syncId' // sync report 편집
+    };
+
+    public static ROOT = '/' + HOME.ROUTE.ROOT + '/' + SYNC.ROUTE.ROOT;
+    public static SYNC_DETAIL = '/' + HOME.ROUTE.ROOT + '/' + SYNC.ROUTE.DETAIL;
+    public static SYNC_EDIT = '/' + HOME.ROUTE.ROOT + '/' + SYNC.ROUTE.EDIT;
+  }
+
+  // 리포트 만들기 -> 스냅 리포트
+  export class SNAP {
+    public static ROUTE = {
+      ROOT: 'snap',
+      DETAIL: 'detail/:snapId', // 특정 snap report 열람
+      EDIT: 'edit/:snapId' // sync report 편집
+    };
+
+    public static ROOT = '/' + HOME.ROUTE.ROOT + '/' + SNAP.ROUTE.ROOT;
+    public static SNAP_DETAIL = '/' + HOME.ROUTE.ROOT + '/' + SNAP.ROUTE.DETAIL;
+    public static SNAP_EDIT = '/' + HOME.ROUTE.ROOT + '/' + SNAP.ROUTE.EDIT;
   }
 
   export class USER {
@@ -24,22 +49,8 @@ export namespace UrlPath {
       PROFILE: 'user-profile'
     };
 
-    public static USER_PROFILE = '/' + TEMPLATE.ROUTE.ROOT + '/' + USER.ROUTE.PROFILE;
+    public static USER_PROFILE = '/' + HOME.ROUTE.ROOT + '/' + USER.ROUTE.PROFILE;
     public static USER_SIGNIN = '/' + USER.ROUTE.ROOT + '/' + USER.ROUTE.SIGNIN;
-  }
-
-  export class SYNC {
-    public static ROUTE = {
-      ROOT: 'sync',
-      SYNC_DETAIL: 'detail/:syncId'
-    };
-  }
-
-  export class SNAP {
-    public static ROUTE = {
-      ROOT: 'snap',
-      SYNC_DETAIL: 'detail/:snapId'
-    };
   }
 
 }
